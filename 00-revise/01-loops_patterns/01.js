2ce977c904f4320444b39a09e7896663d6b6c264
Aditya Patil <aadityapatil8012@gmail.com>
2024-09-06 01:01:03 +0530

    next start with arrays

--- 00-revise/01-loops_patterns/01.js
+++ 00-revise/01-loops_patterns/01.js
@@ -1,5 +1,4 @@
-// Number patterns: 
-
+// Number patterns:
 
 // let pattern = ""; // output: none;
 
@@ -11,14 +10,11 @@
 // pattern = pattern + 2; // output: 12
 // console.log(pattern);
 
-
 // print the triangle pattern with numbers;
 
 // this will run the outer loop
 
-
 function triangleNumberPattern1(rows) {
-
   let col = ""; // In this the no. from the inner loop will be added;
   for (let i = 1; i <= rows; i++) {
     for (let num = 1; num <= i; num++) {
@@ -32,19 +28,52 @@ function triangleNumberPattern1(rows) {
 
 // triangleNumberPattern1(6);
 
-
 function triangleNumberPattern2(rows) {
   let pattern = "";
-  for(let i= 1; i<=rows; i++){
-    for(let num = 1; num<=i; num++){
+  for (let i = 1; i <= rows; i++) {
+    for (let num = 1; num <= i; num++) {
       pattern = pattern + i;
     }
 
-    pattern = pattern + "\n"
+    pattern = pattern + "\n";
   }
   console.log(pattern);
-  
 }
 
-triangleNumberPattern2(6)
-triangleNumberPattern2(5)
+// triangleNumberPattern2(6)
+// triangleNumberPattern2(5)
+
+function triangleNumberPattern3(rows) {
+  let variable = 1;
+  let pattern = "";
+  for (let i = 1; i <= rows; i++) {
+    for (let num = 1; num <= i; num++) {
+      pattern = pattern + variable + " ";
+      variable++;
+    }
+
+    pattern = pattern + "\n";
+  }
+  console.log(pattern);
+}
+
+// triangleNumberPattern3(6)
+
+// now let's start with number pyramid pattern-1;
+
+// this is kinda difficult for right now and will come to this some time later;
+
+// square star pattern 1;
+
+function starPattern1(number) {
+  let pattern = "";
+  for (let n = 1; n <= number; n++) {
+    for (let star = 1; star <= number; star++) {
+      pattern = pattern + "*";
+    }
+    pattern = pattern + "\n";
+  }
+  console.log(pattern);
+}
+
+starPattern1(6);
