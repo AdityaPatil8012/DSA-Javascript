b9d8e7037f67045605a5e62a56859761de42ba55
Aditya Patil <aadityapatil8012@gmail.com>
2024-09-14 20:03:57 +0530

    Started solving problems but need to learn about .reduce() and .filter() next and then solve the remaining problems

---
+++ 01-arrays/09.js
@@ -0,0 +1,34 @@
+// Given an array of integers nums and an integers target,
+// return the indics of the two numbers such that they add up to target;
+
+// for this let loop it first;
+
+const nums = [1,2,3,4,5,6];
+
+function twoSum(nums, target) {
+  let ans = []
+  for(let i = 0; i<nums.length; i++){
+    for(let n = i + 1; n<nums.length; n++){
+      if(nums[i]+nums[n] === target){  
+        ans.push(i);
+        ans.push(n);
+        return ans;
+      }
+    }
+  }
+  return "Ans not found";
+} 
+
+console.log(twoSum(nums, 9))
+
+// let ans = [];
+
+// console.log(ans);
+
+// ans.push(1);
+// console.log(ans);
+// ans.push(2);
+// console.log(ans);
+
+
+
---
+++ 01-arrays/10.js
@@ -0,0 +1,5 @@
+// find the majority element; 
+
+// First we need to sort;
+// After that we need to filter the result;
+// Then we have to reduce;
