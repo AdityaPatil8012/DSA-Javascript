148cab61050287c37066530a9b74d3a884733a0c
Aditya Patil <aadityapatil8012@gmail.com>
2024-09-23 18:03:27 +0530

    Started solving string problem but, I need to understand pointers

---
+++ 03-strings/05.js
@@ -0,0 +1,18 @@
+// Longest Common Prefix
+
+
+
+let longestCommonPrefix = function(arr) {
+  for(let str of arr){
+    console.log(str);
+    
+  }
+}
+
+let strs = ["flower", "flow", "flight"];
+
+// longestCommonPrefix(strs)
+
+// this to not an easy one to solve;
+
+// will come back to this later;
