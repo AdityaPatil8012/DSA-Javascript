65592a408460991f1c2acb7d1d1cb6253e621721
Aditya Patil <aadityapatil8012@gmail.com>
2024-09-28 20:26:46 +0530

    solved one problem, because I have other work to finish

---
+++ 03-strings/08.js
@@ -0,0 +1,8 @@
+// check if the sentence is palindrome;
+
+phrase1 = "A man"
+phrase2 = "a plan";
+
+// first we need to make the words in lower case;
+// remove the empty spaces and non-alpha
+// then sort it alphabatically
---
+++ 03-strings/09.js
@@ -0,0 +1,14 @@
+// reverse the words in a string;
+
+let input = "the sky is blue";
+
+let words = input.split(" ");
+
+let reverseWords = words.reverse()
+
+reverseWords
+
+let wordsJoin = reverseWords.join(" ");
+
+wordsJoin
+
