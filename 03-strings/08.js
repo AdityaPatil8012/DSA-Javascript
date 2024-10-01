6b5034a9a8474a4f47311134213a3dfbcd5875e2
Aditya Patil <aadityapatil8012@gmail.com>
2024-10-02 00:14:38 +0530

    solved most problems now in string and now I am starting with recursion

--- 03-strings/08.js
+++ 03-strings/08.js
@@ -1,8 +1,38 @@
 // check if the sentence is palindrome;
 
-phrase1 = "A man"
-phrase2 = "a plan";
 
-// first we need to make the words in lower case;
-// remove the empty spaces and non-alpha
-// then sort it alphabatically
+let sentence = "A man, a plan, a canal: Panama";
+
+// first let's convert the sentence to lowercase;
+
+let lowerCaseSentence = sentence.toLowerCase();
+
+lowerCaseSentence
+
+// now let's remove the spaces and non - alphanumeric characters;
+
+let removeSpaceAnd = lowerCaseSentence.trim().replace(/[^a-zA-Z0-9]/g, '')
+
+removeSpaceAnd
+
+// Now we have to convert the string into arrays;
+
+let convertToArray = removeSpaceAnd.split("")
+
+convertToArray
+
+let reverseArray = convertToArray.reverse();
+
+reverseArray
+
+if(convertToArray === reverseArray){
+  console.log("given string is palindrome");
+  
+}
+
+// let's learn and understand more about slice() methods;
+
+let firstName = "Aditya";
+
+let slice = firstName.slice();
+slice
